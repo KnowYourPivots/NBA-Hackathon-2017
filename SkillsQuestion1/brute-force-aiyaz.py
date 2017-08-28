cbbc20a36fb5918d8e6d12c0a9ff6bf4d434d924
AiyazN25 <aiyaz.noor@mail.utoronto.ca>
2017-08-27 22:32:26 -0400

    Updated with proper analytic recursive solution in brute-force-aiyaz.py

--- SkillsQuestion1/brute-force-aiyaz.py
+++ SkillsQuestion1/brute-force-aiyaz.py
@@ -1,4 +1,5 @@
 from __future__ import division
+import math
 
 def list_of_all_games_outcomes(n):
     if n == 0:
@@ -52,13 +53,47 @@ def probability_of_two_consecutive_losses_in_n_games(n):
         outcome_probability = 1.0
 
     return total_probability
+
+
+def recursive_function_solution(n):
+    if(n <= 1):
+        return 0
+    else:
+        # Formula: F(n) = P_W*F(n-1) + (1-P_W)^2 + (1-P_W)*P_W*F(n-2)
+        return P_W*(recursive_function_solution(n-1)) + math.pow(1-P_W, 2) + (1-P_W)*P_W*(recursive_function_solution(n-2))
+
+
+def return_list_of_probabilities(n):
+    probabilites_list = [0, 0]
+    for i in range (n-1):
+        probability = P_W*(probabilites_list[-1]) + math.pow(1-P_W, 2) + (1-P_W)*P_W*(probabilites_list[-2])
+        probabilites_list.append(round(probability, 4))
+    
+    return probabilites_list
+
+
+def probability_of_two_consecutive_losses_SOLUTION(n):
+    F_n_minus_1, F_n_minus_2 = 0.0, 0.0 
+    for i in range (n-1):
+        probability = P_W*(F_n_minus_1) + math.pow(1-P_W, 2) + (1-P_W)*P_W*(F_n_minus_2)
+        #probability = round(probability, 4)
+        F_n_minus_2 = F_n_minus_1
+        F_n_minus_1 = probability
+    
+    return probability
     
 
 if __name__ == "__main__":
     #print(list_of_all_games_outcomes(1))
     #print(list_of_all_games_outcomes(2))
-    print(list_of_all_games_outcomes(4))
-    print(probability_of_two_consecutive_losses_in_n_games(4))
-    print(non_LL_outcomes_given_x_Ws_and_y_Ls(3, 3, 6))
-    print(non_LL_outcomes_given_x_Ws_and_y_Ls(4, 2, 6))
-            
+    #print(list_of_all_games_outcomes(4))
+    #print(probability_of_two_consecutive_losses_in_n_games(4))
+    #print(non_LL_outcomes_given_x_Ws_and_y_Ls(3, 3, 6))
+    #print(non_LL_outcomes_given_x_Ws_and_y_Ls(4, 2, 6))
+    n = 82
+    P_W = 0.9037
+    #print(probability_of_two_consecutive_losses_in_n_games(n))
+    #print(recursive_function_solution(n))
+    #print(return_list_of_probabilities(n))
+    print(probability_of_two_consecutive_losses_SOLUTION(n))
+    
